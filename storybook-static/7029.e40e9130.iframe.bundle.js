e19a394a3296308fed41f8cd7a67e444b70932dd
bomee.yoon <bomee.yoon@godo.co.kr>
2023-05-02 12:14:03 +0900

    chore: 스토리북 빌드

--- storybook-static/7029.e40e9130.iframe.bundle.js
+++ storybook-static/7029.e40e9130.iframe.bundle.js
@@ -1,10 +1,1 @@
-'use strict';
-(self.webpackChunk_shopby_react_components = self.webpackChunk_shopby_react_components || []).push([
-  [7029],
-  {
-    '../../node_modules/react-dom/client.js': (__unused_webpack_module, exports, __webpack_require__) => {
-      var m = __webpack_require__('../../node_modules/react-dom/index.js');
-      (exports.createRoot = m.createRoot), (exports.hydrateRoot = m.hydrateRoot);
-    },
-  },
-]);
+"use strict";(self.webpackChunk_shopby_react_components=self.webpackChunk_shopby_react_components||[]).push([[7029],{"../../node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("../../node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
